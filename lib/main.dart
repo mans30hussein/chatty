05800a7e5c92017f46845f5be90d9fca9ac84a5e
30102091900132 <mohamedhusseinmans@gmail.com>
2023-12-23 21:32:25 +0300

    updata

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,14 @@
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
-import 'package:chatty/presentaion/screens/homeScreen.dart';
-import 'package:chatty/presentaion/screens/logIn.dart';
-import 'package:chatty/presentaion/screens/register.dart';
-
+// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
 import 'package:flutter/material.dart';
+
+import 'package:firebase_core/firebase_core.dart';
+
+import 'auth/login.dart';
+import 'auth/signup.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
@@ -23,40 +20,14 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  // This widget is the root of your application.
- String? password ;
- String? userName ;
- String? email ;
-  @override
-  void initState() {
-    // TODO: implement initState
-    FirebaseAuth.instance.authStateChanges().listen((User? user) {
-      if (user == null) {
-        print('*****************************************User is currently signed out!');
-      } else {
-        print('********************************************User is signed in!');
-      }
-    });
-    super.initState();
-  }
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: LogIn(),
+      debugShowCheckedModeBanner: false,
+      home: Login(),
       routes: {
-      'register' : (context){
-        return RegisterScreen(
-          email:email.toString(),
-          password: password.toString(),
-          username: userName.toString(),
-        ) ;
-      },
-        'homeScreen' : (context){
-          return HomeScreen() ;
-        },
-        'loginScreen' : (context){
-          return LogIn() ;
-        },
+        "signup" : (context) => SignUp() , 
+        "login" : (context) => Login()
       },
     );
   }
